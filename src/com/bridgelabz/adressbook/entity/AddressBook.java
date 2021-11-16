29cb4694a062879851f8fd5936a4a9c4c3540ee3
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-16 10:58:49 +0530

    adding welcome message in main method

---
+++ src/com/bridgelabz/adressbook/entity/AddressBook.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.adressbook.entity;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+
+		System.out.println("----------Welcome to Address Book Program-----------");
+
+	}
+
+}
